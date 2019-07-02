89b77e12fffd438b043fe83457195228213afa30
cliffpham <cliffpham@gmail.com>
2019-07-02 00:11:40 -0700

    building login/logout functionality

--- app/models.py
+++ app/models.py
@@ -1,5 +1,9 @@
 import datetime, re
-from app import db
+from app import db, login_manager, bcrypt
+
+@login_manager.user_loader
+def _user_loader(user_id):
+    return User.query.get(int(user_id))
 
 def slugify(str):
     return re.sub('[^\w]+', '-', str).lower()
@@ -58,6 +62,55 @@ class Tag(db.Model):
     def __repr__(self):
         return '<Tag %s>' % self.name
 
+class User (db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(255))
+    name = db.Column(db.String(64))
+    slug = db.Column(db.String(64), unique=True)
+    active = db.Column(db.Boolean, default=True)
+    created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __init__(self, *args, **kwargs):
+        super(User, self).__init__(*args, **kwargs)
+        self.generate_slug()
+
+    def generate_slug(self):
+        if self.name:
+            self.slug = slugify(self.name)
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    @staticmethod
+    def make_password(plaintext):
+        return bcrypt.generate_password_hash(plaintext)
+
+    def check_password(self, raw_password):
+        return bcrypt.check_password_hash(self.password_hash, raw_password)
+
+    @classmethod
+    def create(cls, email, password, **kwargs):
+        return User(
+            email = email,
+            password_hash = User.make_password(password), **kwargs)
+
+    @staticmethod
+    def authenticate(email, password):
+        user = User.query.filter(User.email == email).first()
+        if user and user.check_password(password):
+            return user
+        return False
+
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64))
@@ -69,3 +122,5 @@ class Category(db.Model):
 
     def __repr__(self):
         return '<Category %s>' % self.name
+
+
